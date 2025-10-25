20e617e616070c78b806eaee1e6db4666e46e340
unknown <asjerk00@gmail.com>
2025-10-24 22:55:04 -0700

    feat: - Llevo el EDA a producción con scripts y graficos automaticos, en esta ocasión mejore mi automatizacion del codigo utils.py y python.py para que generara y guardara la data procesada, los graficos y los modelos de forma automatica usando pickle. - Ademas aplique el modelo de arboles de dedciciones a una data para predecir diabetes, use metricas de precision acuaraccy y mean square error

--- src/app.py
+++ src/app.py
@@ -1,4 +1,41 @@
-from utils import db_connect
-engine = db_connect()
+from utils import load_data, processed_data, split_data, save_model, graphics
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, mean_squared_error
+from sklearn import tree
+import matplotlib.pyplot as plt
 
-# your code here
+import pickle
+
+# Cargar datos
+url = "https://breathecode.herokuapp.com/asset/internal-link?id=930&path=diabetes.csv"
+df = load_data(url)
+
+# Preparar datos
+data_processed = processed_data(df)
+
+# Dividir datos
+X_train, X_test, y_train, y_test = split_data(data_processed, 'Outcome')
+
+# Inicializar y entrenar el modelo
+print(f"Entrenando modelo de arboles de decisiones")
+model_decision_tree = DecisionTreeClassifier(random_state=42)
+model_decision_tree.fit(X_train, y_train)
+print("Modelo entrenado correctamente")
+# Realizar predicciones
+y_pred_tree = model_decision_tree.predict(X_test)
+
+# Evaluar el modelo
+print(f"Evaluando modelo de arboles de decisiones")
+accuracy = accuracy_score(y_test, y_pred_tree)
+mse = mean_squared_error(y_test, y_pred_tree)
+print(f"Modedlo evaluado correctamente, acuaracy y mse")
+print(f"Accuracy: {accuracy}")
+print(f"Mean Squared Error: {mse}")
+
+# Guardar modelo
+
+# Graficos
+graphics(data_processed)
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,89 @@
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+import matplotlib.pyplot as plt
+import pickle
 
-# load the .env file variables
-load_dotenv()
+# Funciones para cargar datos y preparar datos
+
+def load_data(url):
+    """
+
+    :param url: Url con la data .csv
+    :return: un dataframe de pandas
+    """
+    print(f"Cargando datos desde la URL: {url}")
+    df = pd.read_csv(url)
+    print(f"Datos cargados correctamente")
+    return df
+
+# Función de limpieza y preparación de datos
+
+def processed_data(df):
+    """
+
+    :param df: Dataframe de pandas
+    :return: Data frame de pandas limpio y listo para el modelo
+    """
+    # Realizo una copia del data frame
+
+    data_processed = df.copy()
+    print(f"Copia del dataframe realizada correctamente")
+
+    # Busca si hay duplicados y los elimina
+    print(f"Buscando duplicados")
+    if data_processed.duplicated().sum()> 0:
+        data_processed.drop_duplicates(inplace=True)
+        print(f"Duplicados eliminados")
 
 
-def db_connect():
-    import os
-    engine = create_engine(os.getenv('DATABASE_URL'))
-    engine.connect()
-    return engine
+    # Busca si hay nulos oh valores 0 en las columnas ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI'] y los remplaza por nan
+    print(f"Remplazando 0 con nan en las columnas seleccionadas")
+    cols_con_ceros = ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']
+
+    # Reemplazo 0 con np.nan en esas columnas
+    data_processed[cols_con_ceros] = data_processed[cols_con_ceros].replace(0, np.nan)
+
+    print("Remplazando valores nan con la media de cada columna")
+
+    # Imputación de nulos con la media de cada columna
+    data_processed = data_processed.fillna(data_processed.mean())
+    print("Valores remplazados correctamente")
+
+    # Ahora nos quedamos con la data limpia para su posterior entrenamiento
+    data_model = data_processed[['Glucose', 'BMI', 'Age', 'Pregnancies', 'Outcome']]
+
+    return data_model
+
+def split_data(df, var_obj):
+    """
+
+    :param df: DataFrame listo para el modelo
+    :param var_obj: Variable objetivo y
+    :return: data dividida en entrenamiento y prueba
+    """
+    # Selecciono la data para X
+    X = df.drop('Outcome', axis=1)
+
+    # Selecciono solo la variable objetivo en este caso Outcome para y
+    y = df['Outcome']
+    # Utilizo train_test_split para dividir la data y entrenarla
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+def save_model(model, path):
+    """
+
+    :param model: modelo
+    :param path: ruta de guardado
+    :return: Guarda el modelo.pkl en la ruta indicada
+    """
+    return pickle.dump(model, path)
+
+# Funcion para graficas
+
+def graphics(data_processed):
+    grafico = pd.plotting.parallel_coordinates(data_processed, 'Outcome', color=('orange', 'cyan'))
+    return grafico
+
